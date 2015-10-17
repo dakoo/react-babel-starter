9d93e00bc41bfb29b70daaec4e19b907960b89d3
Hochul Shin <darkblue99@gmail.com>
2015-10-17 23:15:23 +0900

    first commit

---
+++ app/app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Greeting from "./greeting";
+import Style from "./style.css";
+
+React.render(<Greeting name="World" />, document.querySelector(".container"));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	context: __dirname + "/app",
+	entry: {
+		javascript: "./app.js",
+		html: "./index.html"
+	},
+	
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loaders: ["react-hot", "babel-loader"],
+			},
+			{
+				test: /\.html$/,
+				loader: "file?name=[name].[ext]",
+			},
+			{
+				test: /\.css$/,
+				loader: "style!css",
+			},
+		]
+	},
+	
+	output: {
+		filename: "app.js",
+		path: __dirname + "/dist"
+	}	
+}
